7de823c83206ce38a1987c1183bdddeaca11e314
ripuns <ripunsethia1668@gmail.com>
2026-03-26 17:03:54 +0530

    feat: added a classfier to sort text into appropriate language

---
+++ classification/classifier.py
@@ -0,0 +1,14 @@
+import numpy as np
+from PIL import Image
+
+class TextClassifier:
+    def classify(self, image):
+        gray = image.convert("L")
+        arr = np.array(gray)
+
+        if np.std(arr) > 70:
+            return "handwritten"
+        elif np.mean(arr) < 150:
+            return "chinese"
+        else:
+            return "printed"
