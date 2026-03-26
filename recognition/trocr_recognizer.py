d44c45343ecb017db90414d2212623d1803bc2a7
ripuns <ripunsethia1668@gmail.com>
2026-03-26 17:05:44 +0530

    feat: added TrOCR text recognition model to get correct text from images

---
+++ recognition/trocr_recognizer.py
@@ -0,0 +1,27 @@
+import torch
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
+
+class TrOCRRecognizer:
+    def __init__(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.models = {
+            "printed": self.load_model("microsoft/trocr-large-printed"),
+            "handwritten": self.load_model("microsoft/trocr-large-handwritten"),
+            "chinese": self.load_model("ZihCiLin/trocr-traditional-chinese-baseline")
+        }
+
+    def load_model(self, name):
+        processor = TrOCRProcessor.from_pretrained(name)
+        model = VisionEncoderDecoderModel.from_pretrained(name).to(self.device)
+        return processor, model
+
+    def recognize(self, image, label):
+        processor, model = self.models[label]
+
+        pixel_values = processor(images=image, return_tensors="pt").pixel_values.to(self.device)
+        generated_ids = model.generate(pixel_values)
+
+        text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return text
